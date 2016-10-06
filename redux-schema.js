1db2f4da978076f1960d25e7e44c59b98ae39b56
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-06 19:09:01 +0200

    feat: add initial redux schema

---
+++ redux-schema.js
@@ -0,0 +1,74 @@
+{
+  users: {
+    isAuthed,
+    isFetching,
+    error,
+    authedId,
+    [uid]: {
+      lastUpdated,
+      info: {
+        name,
+        uid,
+        avatar
+      }
+    }
+  },
+  modal: {
+    duck,
+    isOpen
+  },
+  ducks: {
+    [duckId]: {
+      lastUpdated,
+      info: {
+        avatar,
+        duckId,
+        name,
+        text,
+        timestamp,
+        uid
+      }
+    }
+  },
+  likeCount: {
+    [duckId]: 0
+  },
+  usersDucks: {
+    isFetching,
+    error,
+    [uid]: {
+      lastUpdated,
+      duckIds: [duckId, duckId, duckId]
+    }
+  },
+  usersLikes: {
+    [duckId]: true
+  },
+  feed: {
+    isFetching,
+    error,
+    newDucksAvailable,
+    duckIdsToAdd: [duckId, duckId],
+    duckIds: [duckId, duckId, duckId]
+  },
+  replies: {
+    isFetching,
+    error,
+    [duckId]: {
+      lastUpdated,
+      replies: {
+        [replyId]: {
+          lastUpdated,
+          name,
+          comment,
+          uid,
+          timestamp,
+          avatar
+        }
+      }
+    }
+  },
+  listeners: {
+    [listenersId]: true
+  },
+}

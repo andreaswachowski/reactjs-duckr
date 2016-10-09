6c26c5cf81dabbf2600ccfbba5523d838c5efb3a
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-09 11:43:32 +0200

    fix(router): change incorrect Route to Router

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Router, Route, hashHistory} from 'react-router';
+import {Router, hashHistory} from 'react-router';
 import {MainContainer} from 'containers';
 
 const routes =
   <Router history={hashHistory}>
-    <Route path='/' component={MainContainer} />
+    <Router path='/' component={MainContainer} />
   </Router>;
 
 export default routes;

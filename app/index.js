7ac6d72070ee1308f16ba600f6edaa7bca733862
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-10 14:18:33 +0200

    feat(redux): make store accessible from components with react-redux

--- app/index.js
+++ app/index.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from './config/routes';
 import {createStore} from 'redux';
 import users from 'redux/modules/users';
+import {Provider} from 'react-redux';
 
 const store = createStore(users, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  routes,
+  <Provider store={store}>
+    {routes}
+  </Provider>,
   document.getElementById('app')
 );

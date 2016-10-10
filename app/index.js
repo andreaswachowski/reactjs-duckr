34fcac5c2aa7cffe32734088f639f8a42bc35da2
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-10 15:50:18 +0200

    refa(AuthenticateContainer): simplify container with redux-thunk

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import routes from './config/routes';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import users from 'redux/modules/users';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 
-const store = createStore(users, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(users, undefined, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>

a72c3335b971b2270e8f3639a37be1c79aa03e6e
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-10 14:13:46 +0200

    feat(redux): add users actions and reducer

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 import ReactDOM from 'react-dom';
 import routes from './config/routes';
+import {createStore} from 'redux';
+import users from 'redux/modules/users';
+
+const store = createStore(users, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   routes,

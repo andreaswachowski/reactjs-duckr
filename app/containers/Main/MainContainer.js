74ea9770fe587111052da5b33ca79acd63141320
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-10 16:47:54 +0200

    feat(MainContainer): pass isAuthed-state to MainContainer

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,15 +1,17 @@
 import React, {PropTypes} from 'react';
 import {Navigation} from 'components';
 import {container, innerContainer} from './styles.css';
+import {connect} from 'react-redux';
 
 const MainContainer = React.createClass({
   propTypes: {
+    isAuthed: PropTypes.bool.isRequired,
     children: PropTypes.node
   },
   render () {
     return (
       <div className={container}>
-        <Navigation isAuthed={false} />
+        <Navigation isAuthed={this.props.isAuthed} />
         <div className={innerContainer}>
           {this.props.children}
         </div>
@@ -18,4 +20,6 @@ const MainContainer = React.createClass({
   }
 });
 
-export default MainContainer;
+export default connect(
+  (state) => ({isAuthed: state.isAuthed}) // inlined mapStateToProps
+)(MainContainer);

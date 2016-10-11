47e21d0d71fba58374d3d7f12d23a14461cb1f81
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-11 10:18:14 +0200

    feat(Authenticate): add Facebook authentication

---
+++ app/config/constants.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyBN0ugTo4IICdLf2m7d-bm_IaTMLOTXPL4',
+  authDomain: 'reactjs-duckr-20f11.firebaseapp.com',
+  databaseURL: 'https://reactjs-duckr-20f11.firebaseio.com',
+  storageBucket: 'reactjs-duckr-20f11.appspot.com',
+  messagingSenderId: '874815677509'
+};
+
+firebase.initializeApp(config);
+
+export const ref = firebase.database().ref();
+export const firebaseAuth = firebase.auth;
--- app/helpers/auth.js
+++ app/helpers/auth.js
@@ -1,13 +1,8 @@
+import firebase from 'firebase';
+import {ref, firebaseAuth} from 'config/constants';
+
 export default function auth () {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({
-        name: 'Andreas Wachowski',
-        avatar: 'http://www.andreas-wachowski.de/images/profile_picture.170254e3.png',
-        uid: 'andreaswachowski'
-      });
-    }, 2000);
-  });
+  return firebaseAuth().signInWithPopup(new firebase.auth.FacebookAuthProvider());
 }
 
 export function checkIfAuthed (store) {
@@ -16,5 +11,11 @@ export function checkIfAuthed (store) {
 }
 
 export function logout () {
-  console.log('Logged out.');
+  return firebaseAuth().signOut();
+}
+
+export function saveUser (user) {
+  return ref.child(`users/${user.uid}`)
+    .set(user) // save user to the location users/${user.uid}
+    .then(() => user); // return the user object passed in, to facilitate chaining
 }

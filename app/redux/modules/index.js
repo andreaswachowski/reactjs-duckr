f9e0b3bf14eaf207bb14d236541e96efd96fd334
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-12 11:56:33 +0200

    feat(redux): add feed reducer

--- app/redux/modules/index.js
+++ app/redux/modules/index.js
@@ -4,3 +4,4 @@ export {default as users} from './users';
 export {default as modal} from './modal';
 export {default as ducks} from './ducks';
 export {default as usersDucks} from './usersDucks';
+export {default as feed} from './feed';

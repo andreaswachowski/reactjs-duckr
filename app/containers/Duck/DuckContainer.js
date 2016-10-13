b948fc8522cc24f10f2a1529596c52fc0c9fde23
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-13 11:32:11 +0200

    feat(DuckContainer): add PropTypes
    hideLikeCount and hideReplyBtn are required because the Duck is
    shown differently on the feed page (no like count, reply button visible)
    and the replies page (no reply button, instead like count).

--- app/containers/Duck/DuckContainer.js
+++ app/containers/Duck/DuckContainer.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import Duck from 'components';
 
+const { func, object, bool, number } = PropTypes;
+
 const DuckContainer = React.createClass({
+  propTypes: {
+    duck: object.isRequired,
+    numberOfLikes: number,
+    isLiked: bool.isRequired,
+    hideLikeCount: bool.isRequired,
+    hideReplyBtn: bool.isRequired,
+    handleDeleteLike: func.isRequired,
+    addAndHandleLike: func.isRequired
+  },
+  getDefaultProps () {
+    return {
+      hideReplyBtn: false,
+      hideLikeCount: true
+    };
+  },
   render () {
     return (
       <Duck />

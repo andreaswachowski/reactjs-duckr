7083faa8cb51519aedd58e6692aae7304041b9a2
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-17 10:19:59 +0200

    feat(Replies): add error handling

--- app/components/Replies/Replies.js
+++ app/components/Replies/Replies.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {errorMsg} from 'sharedStyles/styles.css';
 
-export default function Replies (props) {
+Replies.propTypes = {
+  error: PropTypes.string.isRequired
+};
+
+export default function Replies ({error}) {
   return (
-    <div>{'Replies'}</div>
+    <div>
+      {error ? <h3 className={errorMsg}>{error}</h3> : null}
+    </div>
   );
 }

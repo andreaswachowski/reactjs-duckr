cd323f77b7ba6f9328a334112294eb30df6f1ce0
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-17 13:58:55 +0200

    feat(Replies): more styling, also connect to state

--- app/components/Replies/Replies.js
+++ app/components/Replies/Replies.js
@@ -1,14 +1,29 @@
 import React, {PropTypes} from 'react';
-import {errorMsg} from 'sharedStyles/styles.css';
+import {avatar, replyContainer, header,
+  cushion, center, author} from './styles.css';
+import {formatTimestamp} from 'helpers/utils';
+import {errorMsg, subHeader} from 'sharedStyles/styles.css';
 
 Replies.propTypes = {
-  error: PropTypes.string.isRequired
+  isFetching: PropTypes.bool.isRequired,
+  error: PropTypes.string.isRequired,
+  replies: PropTypes.object
 };
 
-export default function Replies ({error}) {
+export default function Replies ({isFetching, error, replies}) {
+  const replyIds = Object.keys(replies);
   return (
     <div>
       {error ? <h3 className={errorMsg}>{error}</h3> : null}
+      {isFetching === true
+       ? <p className={subHeader}>{'Fetching Replies'}</p>
+       : <div>
+           <h1 className={header}>{'Replies'}</h1>
+           {replyIds.map((replyId) => {
+             <Reply key={replyId} comment={replies[replyId]} />;
+           })}
+         </div>}
+         {replyIds.length === 0 ? <h3 className={center}>{'Be the first to comment.'}</h3> : null}
     </div>
   );
 }
--- app/containers/Replies/RepliesContainer.js
+++ app/containers/Replies/RepliesContainer.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
 import {Replies} from 'components';
 
 const RepliesContainer = React.createClass({
+  propTypes: {
+    isFetching: PropTypes.bool.isRequired,
+    error: PropTypes.string.isRequired,
+    lastUpdated: PropTypes.number.isRequired,
+    replies: PropTypes.object.isRequired,
+    duckId: PropTypes.string.isRequired // passed in from DuckDetails
+  },
+  getDefaultProps () {
+    return {
+      lastUpdated: 0,
+      replies: {}
+    };
+  },
   render () {
     return (
-      <Replies />
+      <Replies
+        isFetching={this.props.isFetching}
+        error={this.props.error}
+        lastUpdated={this.props.lastUpdated}
+        replies={this.props.replies} />
     );
   }
 });
 
-export default RepliesContainer;
+function mapStateToProps (state, props) {
+  const duckRepliesInfo = state.replies[props.duckId] || {};
+  const { lastUpdated, replies } = duckRepliesInfo; // both undefined if duckRepliesInfo=={}
+
+  return {
+    isFetching: state.replies.isFetching,
+    error: state.replies.error,
+    lastUpdated,
+    replies
+  };
+}
+
+export default connect(mapStateToProps)(RepliesContainer);

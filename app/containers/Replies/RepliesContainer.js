2cb3c60b6986d4c2effd1fce7584b34bf3dfb40b
Andreas Wachowski <andreas.wachowski@akra.de>
2016-10-17 10:06:40 +0200

    feat(Replies): add skeleton component and container

---
+++ app/containers/Replies/RepliesContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Replies} from 'components';
+
+const RepliesContainer = React.createClass({
+  render () {
+    return (
+      <Replies />
+    );
+  }
+});
+
+export default RepliesContainer;
